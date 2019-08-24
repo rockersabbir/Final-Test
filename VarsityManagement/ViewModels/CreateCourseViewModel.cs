4c760ebaa6e93bb31aecc9194c2eb0e2e91e3d72
rockersabbier <rockersabbier@gmail.com>
2019-08-24 16:11:20 +0600

    Course CRUD

---
+++ VarsityManagement/ViewModels/CreateCourseViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VarsityManagement.ViewModels
+{
+    public class CreateCourseViewModel
+    {
+        [Required]
+        [Display(Name = "Course=> Name")]
+        public string CourseName { get; set; }
+    }
+}
