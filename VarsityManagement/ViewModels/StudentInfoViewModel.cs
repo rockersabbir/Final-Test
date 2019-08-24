5bf18639c91f9ba9ec119323b1ce90187c1b9bb9
rockersabbier <rockersabbier@gmail.com>
2019-08-24 17:23:36 +0600

    student course add

---
+++ VarsityManagement/ViewModels/StudentInfoViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using VarsityManagement.Models;
+
+namespace VarsityManagement.ViewModels
+{
+    public class StudentInfoViewModel
+    {
+        
+        public Student student { get; set; }
+    }
+}
